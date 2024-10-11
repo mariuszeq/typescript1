f1b7e0b2b0065acfca845235cd0afc64400d52a0
mariuszeq <igormajchrzak6b@gmail.com>
2024-10-11 15:12:10 +0200

    typek.js

---
+++ type.js
@@ -0,0 +1,31 @@
+"use strict";
+let zmienna = "napis";
+let zmienna2 = `costam ${zmienna}`;
+console.log(zmienna);
+let liczba = 12;
+let liczba2 = 12.455;
+let hex = 0xfff;
+let binary = 0b11101;
+let octal = 0o361;
+console.log(`dziesietny: ${hex}, po przecinku: ${liczba2}, binarny: ${binary}, osemkkowy ${octal}`);
+let mezczyzna;
+let kobieta;
+const agnieszka = "kobieta";
+if (agnieszka == "kobieta") {
+    kobieta = true;
+    mezczyzna = false;
+}
+else {
+    kobieta = false;
+    mezczyzna = true;
+}
+console.log(`zmienna mezczyzna ma wartosc ${mezczyzna}, a kobieta ma wartosc ${kobieta}`);
+let cokolwiek;
+cokolwiek = "szkola";
+console.log(cokolwiek);
+cokolwiek = 45;
+console.log(cokolwiek);
+let wspolna;
+wspolna = 123;
+wspolna = "zmienna moze przyjac rozne wartosci";
+console.log(wspolna);
